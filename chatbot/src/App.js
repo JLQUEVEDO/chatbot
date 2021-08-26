a6a68128cfbb5dfa393d38c43530c67c363cb146
Jose Quevedo <jlquevedo3@gmail.com>
2021-08-25 23:02:07 -0600

    get conversation from app

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 //importing the component Frame
 import {Frame} from "./components/Frame"
+import { getConversation } from './data/getConversation';
 
 function App() {
+  useEffect(()=>{
+    getConversation()
+  },[]);
   return (
     <div className="App">
      <Frame />
