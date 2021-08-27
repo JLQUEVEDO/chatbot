3494f0941c4fbf9d61bd87e3a1cebaaffd3b5618
Jose Quevedo <jlquevedo3@gmail.com>
2021-08-26 21:24:15 -0600

    fixing cors

--- server/src/index.js
+++ server/src/index.js
@@ -8,9 +8,7 @@ app.use(cors())
 
 
 app.get('/', (req, res) => {
-  //res.header('Access-Control-Allow-Origin','*'); //added 
-  //res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   res.send('Hello World!')
 })
 
